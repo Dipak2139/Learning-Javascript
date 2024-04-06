a50a44d091c183d5b3374f058d288a5148ad126d
Dipak2139 <dipakdas7478@gmail.com>
2024-04-06 09:00:35 +0530

    Starting javascript

---
+++ Starting/new.js
@@ -0,0 +1,20 @@
+alert("Warning PLease leave...")
+
+console.log("Code is running")
+console.log("Code is also running")
+console.log("Code is 3rd time running")
+
+
+//creating variable to store values
+
+var a = prompt("Enter your number")
+console.log("Your value is " + a)
+
+var istrue = confirm("Are you sure you are 18+")
+
+if(istrue){
+    console.log("You will get a voter id card")
+}
+else{
+    console.log("You will not get a voter id card")
+}
