8dcf071fd316623320f2464c52e7e70ef8e5cdbe
Dipak2139 <dipakdas7478@gmail.com>
2024-04-06 09:03:25 +0530

    Final commit

---
+++ variable/script.js
@@ -0,0 +1,39 @@
+// Creating variables
+// var a = 10;
+// var b = 20;
+// console.log(a,b)
+
+// // instead of using var we will use let for specifying variable
+
+// let c = 100;
+// let d = 200;
+// console.log(c,d)
+
+// {
+//     let a = 200; // let is block level element and var is a global level variable
+//     console.log(a);
+// }
+
+// console.log(a)
+
+// Types of Data types
+//1. Primitive type
+let a = 100; //number
+let b = "Dipak";
+let c = undefined;
+let d = null;
+let e = true;
+
+console.log(a,b,c,d,e)
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e)
+
+//2. Objects
+// Object are stored in key pairs
+let o = {
+    "name" : "Dipak Das",
+    "age":21,
+    "favourite food": "Aloo Posto"
+
+}
+
+console.log(o)
